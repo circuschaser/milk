cb2fa70d78200d6035b167821629fce3e4ef0e01
circuschaser <rob@circuschaser.com>
2013-11-08 17:57:33 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Milk::Application.config.secret_token = '4ab7b23c0f36068b8c2f89a88061ed12fbce7ecb781ecc83ce64e02ef0b5bb9c7c305f123fc3ebc0f23de558e20d95e43d384e756bbd796918f182a32ca48f61'
